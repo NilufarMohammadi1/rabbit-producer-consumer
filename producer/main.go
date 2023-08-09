2f361fe4d8f2f893a1dd8f6a17fe1ab65d941270
Nilufar Mohmmadi <nilufar.mohammadi@snapp.cab>
2023-08-09 19:28:29 +0330

    initial commit

---
+++ producer/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+	"time"
+)
+
+// Here we set the way error messages are displayed in the terminal.
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	time.Sleep(time.Second * 5)
+	// Here we connect to RabbitMQ or send a message if there are any errors connecting.
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
+	// rabbitmq := os.Getenv("RABBITMQ_HOST")
+	// fmt.Println("RabbitMQ Host:", rabbitmq)
+	// failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	// We create a Queue to send the message to.
+	q, err := ch.QueueDeclare(
+		"golang-queue", // name
+		false,          // durable
+		false,          // delete when unused
+		false,          // exclusive
+		false,          // no-wait
+		nil,            // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	// We set the payload for the message.
+	body := "Golang is awesome - Keep Moving Forward!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	// If there is an error publishing the message, a log will be displayed in the terminal.
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Congrats, sending message: %s", body)
+}
